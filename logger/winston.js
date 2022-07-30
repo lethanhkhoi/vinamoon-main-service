8265a476b5ff2e2e8967b88d0fce2921d2be65c8
Thanh Hoang <thanhhoang4869@gmail.com>
2022-07-30 13:37:58 +0700

    log to files

--- logger/winston.js
+++ logger/winston.js
@@ -1,15 +1,61 @@
 const winston = require("winston");
-const { combine, timestamp, json } = winston.format;
+const { combine, timestamp, json, errors } = winston.format;
+
+// const logger = winston.createLogger({
+//   level: "http",
+//   format: combine(
+//     colorize({ all: true }),
+//     timestamp({
+//       format: "MMM-DD-YYYY hh:mm:ss",
+//     }),
+//     printf((info) => `[${info.timestamp}] ${info.level}: ${info.message}`)
+//   ),
+//   transports: [new winston.transports.Console()],
+// });
+
+const errorFilter = winston.format((info, opts) => {
+  return info.level === "error" ? info : false;
+});
+
+const infoFilter = winston.format((info, opts) => {
+  return info.level === "info" ? info : false;
+});
 
 const logger = winston.createLogger({
   level: "http",
-  format: combine(
-    timestamp({
-      format: "YYYY-MM-DD hh:mm:ss.SSS A",
+  format: combine(errors({ stack: true }), timestamp(), json()),
+  transports: [
+    new winston.transports.File({
+      dirname: "logs",
+      filename: "combined.log",
     }),
-    json()
-  ),
-  transports: [new winston.transports.Console()],
+    new winston.transports.File({
+      dirname: "logs",
+      filename: "app-error.log",
+      level: "error",
+      format: combine(errorFilter(), timestamp(), json()),
+    }),
+    new winston.transports.File({
+      dirname: "logs",
+      filename: "app-info.log",
+      level: "info",
+      format: combine(infoFilter(), timestamp(), json()),
+    }),
+  ],
+  exceptionHandlers: [
+    new winston.transports.File({
+      dirname: "logs",
+      filename: "exception.log",
+    }),
+  ],
+  rejectionHandlers: [
+    new winston.transports.File({
+      dirname: "logs",
+      filename: "rejections.log",
+    }),
+  ],
 });
+
+logger.exitOnError = false;
 
 module.exports = logger;
